8833189e3b779d7f306921f56062c229d18b74ee
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2019-10-15 14:35:23 +0200

    Add command for getting internal representation in unicode

---
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/GetInternalRepresentationPrettyPrintUnicodeCommand.java
@@ -0,0 +1,28 @@
+package de.prob.animator.command;
+
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.PrologTerm;
+
+public class GetInternalRepresentationPrettyPrintUnicodeCommand extends AbstractCommand {
+	private static final String PROLOG_COMMAND_NAME = "get_pretty_print_unicode";
+	public static final String VARIABLE = "PP";
+	private String pp;
+
+	@Override
+	public void processResult(
+			final ISimplifiedROMap<String, PrologTerm> bindings) {
+		pp = PrologTerm.atomicString(bindings
+				.get(VARIABLE));
+	}
+
+	@Override
+	public void writeCommand(final IPrologTermOutput pto) {
+		pto.openTerm(PROLOG_COMMAND_NAME).printVariable(VARIABLE)
+				.closeTerm();
+	}
+
+	public String getPrettyPrint() {
+		return pp;
+	}
+}
