0eaa507658eb1eb751b713f218797389a0de7331
dgelessus <dgelessus@users.noreply.github.com>
2020-04-09 12:34:41 +0200

    Rename ExpandedFormula.name field to label
    This is a purely internal change - the public getter was already called
    getLabel().

--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/ExpandedFormula.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/ExpandedFormula.java
@@ -10,13 +10,13 @@ import de.prob.prolog.term.CompoundPrologTerm;
 import de.prob.statespace.StateSpace;
 
 public class ExpandedFormula {
-	private final String name;
+	private final String label;
 	private final BVisual2Value value;
 	private final BVisual2Formula formula;
 	private final List<ExpandedFormula> children;
 
-	public ExpandedFormula(final String name, final BVisual2Value value, final BVisual2Formula formula, final List<ExpandedFormula> children) {
-		this.name = name;
+	public ExpandedFormula(final String label, final BVisual2Value value, final BVisual2Formula formula, final List<ExpandedFormula> children) {
+		this.label = label;
 		this.value = value;
 		this.formula = formula;
 		this.children = children;
@@ -24,17 +24,17 @@ public class ExpandedFormula {
 
 	public static ExpandedFormula fromPrologTerm(final StateSpace stateSpace, final CompoundPrologTerm cpt) {
 		BindingGenerator.getCompoundTerm(cpt, "formula", 4);
-		final String name = cpt.getArgument(1).getFunctor();
+		final String label = cpt.getArgument(1).getFunctor();
 		final BVisual2Value result = BVisual2Value.fromPrologTerm(cpt.getArgument(2));
 		final BVisual2Formula formula = BVisual2Formula.fromFormulaId(stateSpace, cpt.getArgument(3).getFunctor());
 		final List<ExpandedFormula> children = BindingGenerator.getList(cpt.getArgument(4)).stream()
 			.map(pt -> ExpandedFormula.fromPrologTerm(stateSpace, BindingGenerator.getCompoundTerm(pt, "formula", 4)))
 			.collect(Collectors.toList());
-		return new ExpandedFormula(name, result, formula, children);
+		return new ExpandedFormula(label, result, formula, children);
 	}
 
 	public String getLabel() {
-		return name;
+		return label;
 	}
 
 	public BVisual2Value getValue() {
@@ -60,7 +60,7 @@ public class ExpandedFormula {
 	@Override
 	public String toString() {
 		return MoreObjects.toStringHelper(this)
-			.add("name", this.getLabel())
+			.add("label", this.getLabel())
 			.add("value", this.getValue())
 			.add("formula", this.getFormula())
 			.add("children", this.getChildren())
