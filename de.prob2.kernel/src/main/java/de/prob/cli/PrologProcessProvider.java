67d6bbc7c16d80244e657ffb4da946b686e2e5c8
dgelessus <dgelessus@users.noreply.github.com>
2020-05-27 15:10:35 +0200

    Improve top-level error message when failing to start CLI

--- de.prob2.kernel/src/main/java/de/prob/cli/PrologProcessProvider.java
+++ de.prob2.kernel/src/main/java/de/prob/cli/PrologProcessProvider.java
@@ -66,7 +66,7 @@ class PrologProcessProvider implements Provider<ProcessHandle> {
 			prologProcess = pb.start();
 			logger.debug("Cli started");
 		} catch (IOException e) {
-			throw new CliError("Problem while starting up ProB CLI. Tried to execute: " + executable, e);
+			throw new CliError("Problem while starting up ProB CLI: " + e.getMessage(), e);
 		}
 
 		toDestroyOnShutdown.add(prologProcess);
