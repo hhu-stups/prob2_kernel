f3d87097c98ac086559e14a7360731fa67cb2fbb
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2020-08-26 15:00:17 +0200

    Revert commit 8f1c3572

--- de.prob2.kernel/groovyTests/testWDErrorsInEvaluation.groovy
+++ de.prob2.kernel/groovyTests/testWDErrorsInEvaluation.groovy
@@ -1,7 +1,7 @@
-import java.nio.file.Paths
-
-import de.prob.animator.domainobjects.UnknownEvaluationResult
 import de.prob.statespace.Trace
+
+import de.prob.animator.domainobjects.WDError
+import java.nio.file.Paths
 
 final s = api.b_load(Paths.get(dir, "machines", "scheduler.mch").toString())
 def t = new Trace(s)
@@ -9,13 +9,13 @@ t = t.$initialise_machine()
 
 // expression
 final wderror1 = t.evalCurrent("2 / 0")
-assert wderror1 instanceof UnknownEvaluationResult
+assert wderror1 instanceof WDError
 assert wderror1.errors.size == 1
 assert wderror1.errors[0].contains("division by zero")
 
 // predicate
 final wderror2 = t.evalCurrent("3 / 0 = 1")
-assert wderror2 instanceof UnknownEvaluationResult
+assert wderror2 instanceof WDError
 assert wderror2.errors.size == 1
 assert wderror2.errors[0].contains("division by zero")
 
