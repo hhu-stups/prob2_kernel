0176a93c931a1c2509de2174cdd90a5fe7868e20
SeeBasTStick <sebastian.stock@hhu.de>
2020-10-16 08:04:19 +0200

    removed old prototype checker files

---
+++ de.prob2.kernel/src/main/java/de/prob/check/tracereplay/check/CheckType.java
@@ -0,0 +1,14 @@
+package de.prob.check.tracereplay.check;
+
+public class CheckType {
+
+	//Get operations from File
+
+	//Get the currently loaded operations
+
+	//Throw both into evaluation
+
+
+
+
+}
