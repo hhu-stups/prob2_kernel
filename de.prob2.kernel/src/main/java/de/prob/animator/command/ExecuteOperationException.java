0f1b5c8d59452e7ba54be47d866379148f066d3f
dgelessus <dgelessus@users.noreply.github.com>
2020-11-30 15:16:18 +0100

    Fix some indents

--- de.prob2.kernel/src/main/java/de/prob/animator/command/ExecuteOperationException.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/ExecuteOperationException.java
@@ -5,18 +5,18 @@ import java.util.stream.Collectors;
 
 public class ExecuteOperationException extends IllegalArgumentException {
 
-    private List<GetOperationByPredicateCommand.GetOperationError> errors;
+	private List<GetOperationByPredicateCommand.GetOperationError> errors;
 
-    public ExecuteOperationException(String msg, List<GetOperationByPredicateCommand.GetOperationError> errors) {
-        super(msg);
-        this.errors = errors;
-    }
+	public ExecuteOperationException(String msg, List<GetOperationByPredicateCommand.GetOperationError> errors) {
+		super(msg);
+		this.errors = errors;
+	}
 
-    public List<GetOperationByPredicateCommand.GetOperationError> getErrors() {
-        return errors;
-    }
+	public List<GetOperationByPredicateCommand.GetOperationError> getErrors() {
+		return errors;
+	}
 
-    public List<String> getErrorMessages() {
-        return errors.stream().map(GetOperationByPredicateCommand.GetOperationError::getMessage).collect(Collectors.toList());
-    }
+	public List<String> getErrorMessages() {
+		return errors.stream().map(GetOperationByPredicateCommand.GetOperationError::getMessage).collect(Collectors.toList());
+	}
 }
--- de.prob2.kernel/src/main/java/de/prob/animator/command/GetStatisticsCommand.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/GetStatisticsCommand.java
@@ -9,61 +9,61 @@ import java.math.BigInteger;
 
 public class GetStatisticsCommand extends AbstractCommand {
 
-    public enum StatisticsOption {
-        GLOBAL_RUNTIME("global_runtime"),
-        GLOBAL_WALLTIME("global_walltime"),
-        DELTA_RUNTIME("delta_runtime"),
-        DELTA_WALLTIME("delta_walltime"),
-        MEMORY_USED("memory_used"),
-        MEMORY_FREE("memory_free"),
-        GLOBAL_STACK_USED("global_stack_used"),
-        LOCAL_STACK_USED("local_stack_used"),
-        TRAIL_USED("trail_used"),
-        CHOICE_USED("choice_used"),
-        ATOMS_USED("atoms_used"),
-        ATOMS_NBUSED("atoms_nbused"),
-        GC_COUNT("gc_count"),
-        GC_TIME("gc_time");
+	public enum StatisticsOption {
+		GLOBAL_RUNTIME("global_runtime"),
+		GLOBAL_WALLTIME("global_walltime"),
+		DELTA_RUNTIME("delta_runtime"),
+		DELTA_WALLTIME("delta_walltime"),
+		MEMORY_USED("memory_used"),
+		MEMORY_FREE("memory_free"),
+		GLOBAL_STACK_USED("global_stack_used"),
+		LOCAL_STACK_USED("local_stack_used"),
+		TRAIL_USED("trail_used"),
+		CHOICE_USED("choice_used"),
+		ATOMS_USED("atoms_used"),
+		ATOMS_NBUSED("atoms_nbused"),
+		GC_COUNT("gc_count"),
+		GC_TIME("gc_time");
 
-        private final String option;
+		private final String option;
 
-        StatisticsOption(String option) {
-            this.option = option;
-        }
+		StatisticsOption(String option) {
+			this.option = option;
+		}
 
-        public String getOption() {
-            return option;
-        }
-    }
+		public String getOption() {
+			return option;
+		}
+	}
 
-    private static final String PROLOG_COMMAND_NAME = "get_statistics";
+	private static final String PROLOG_COMMAND_NAME = "get_statistics";
 
-    private static final String RESULT = "V";
+	private static final String RESULT = "V";
 
-    private final StatisticsOption option;
+	private final StatisticsOption option;
 
-    private BigInteger result;
+	private BigInteger result;
 
-    public GetStatisticsCommand(StatisticsOption option) {
-        this.option = option;
-    }
+	public GetStatisticsCommand(StatisticsOption option) {
+		this.option = option;
+	}
 
-    @Override
-    public void writeCommand(IPrologTermOutput pto) {
-        pto.openTerm(PROLOG_COMMAND_NAME);
-        pto.printAtom(option.getOption());
-        pto.printVariable(RESULT);
-        pto.closeTerm();
-    }
+	@Override
+	public void writeCommand(IPrologTermOutput pto) {
+		pto.openTerm(PROLOG_COMMAND_NAME);
+		pto.printAtom(option.getOption());
+		pto.printVariable(RESULT);
+		pto.closeTerm();
+	}
 
-    @Override
-    public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
-        IntegerPrologTerm valueTerm = (IntegerPrologTerm) bindings.get("V");
-        result = valueTerm.getValue();
-    }
+	@Override
+	public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
+		IntegerPrologTerm valueTerm = (IntegerPrologTerm) bindings.get("V");
+		result = valueTerm.getValue();
+	}
 
-    public BigInteger getResult() {
-        return result;
-    }
+	public BigInteger getResult() {
+		return result;
+	}
 
 }
