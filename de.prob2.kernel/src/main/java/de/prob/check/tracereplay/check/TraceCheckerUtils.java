b53eefb9a8b6e54d6da22eab0c071f1f703f1184
SeeBasTStick <sebastian.stock@hhu.de>
2020-11-30 15:38:55 +0100

    added a method to create permutations

--- de.prob2.kernel/src/main/java/de/prob/check/tracereplay/check/TraceCheckerUtils.java
+++ de.prob2.kernel/src/main/java/de/prob/check/tracereplay/check/TraceCheckerUtils.java
@@ -1,9 +1,6 @@
 package de.prob.check.tracereplay.check;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TraceCheckerUtils {
 
@@ -26,4 +23,32 @@ public class TraceCheckerUtils {
 		}
 		return Collections.emptyMap();
 	}
+
+
+	/**
+	 * Generate a permutation of a list
+	 * https://stackoverflow.com/questions/10305153/generating-all-possible-permutations-of-a-list-recursively
+	 * @param original the input ist
+	 * @param <E> the type of the permuation
+	 * @return returns all permutations
+	 */
+	public static  <E> List<List<E>> generatePerm(List<E> original) {
+		if (original.isEmpty()) {
+			List<List<E>> result = new ArrayList<>();
+			result.add(new ArrayList<>());
+			return result;
+		}
+		E firstElement = original.remove(0);
+		List<List<E>> returnValue = new ArrayList<>();
+		List<List<E>> permutations = generatePerm(original);
+		for (List<E> smallerPermutated : permutations) {
+			for (int index=0; index <= smallerPermutated.size(); index++) {
+				List<E> temp = new ArrayList<>(smallerPermutated);
+				temp.add(index, firstElement);
+				returnValue.add(temp);
+			}
+		}
+		return returnValue;
+	}
+
 }
