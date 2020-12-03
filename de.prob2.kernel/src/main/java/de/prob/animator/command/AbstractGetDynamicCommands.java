91a8419e94f2f0d959af0b6a84bce0688731e268
dgelessus <dgelessus@users.noreply.github.com>
2020-12-03 16:33:02 +0100

    Simplify AbstractGetDynamicCommands result processing

--- de.prob2.kernel/src/main/java/de/prob/animator/command/AbstractGetDynamicCommands.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/AbstractGetDynamicCommands.java
@@ -1,19 +1,19 @@
 package de.prob.animator.command;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import de.prob.animator.domainobjects.DynamicCommandItem;
+import de.prob.parser.BindingGenerator;
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
-import de.prob.prolog.term.ListPrologTerm;
 import de.prob.prolog.term.PrologTerm;
 import de.prob.statespace.State;
 
 public abstract class AbstractGetDynamicCommands extends AbstractCommand {
 
 	private static final String LIST = "List";
-	private List<DynamicCommandItem> commands = new ArrayList<>();
+	private List<DynamicCommandItem> commands;
 	private final State id;
 	private final String commandName;
 
@@ -32,10 +32,9 @@ public abstract class AbstractGetDynamicCommands extends AbstractCommand {
 
 	@Override
 	public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
-		ListPrologTerm res = (ListPrologTerm) bindings.get(LIST);
-		for (PrologTerm prologTerm : res) {
-			commands.add(DynamicCommandItem.fromPrologTerm(prologTerm));
-		}		
+		commands = BindingGenerator.getList(bindings, LIST).stream()
+			.map(DynamicCommandItem::fromPrologTerm)
+			.collect(Collectors.toList());
 	}
 
 	public List<DynamicCommandItem> getCommands() {
