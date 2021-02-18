fb5128cc91058eba2645612bdb033261421f4d8b
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2021-02-18 12:03:36 +0100

    Implement command for loading VisB file

---
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/LoadVisBCommand.java
@@ -0,0 +1,29 @@
+package de.prob.animator.command;
+
+import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.PrologTerm;
+
+public class LoadVisBCommand extends AbstractCommand {
+
+	private static final String PROLOG_COMMAND_NAME = "prob2_load_visb_file";
+	private final String path;
+
+	public LoadVisBCommand(final String path) {
+		this.path = path;
+	}
+
+	@Override
+	public void writeCommand(final IPrologTermOutput pto) {
+		pto.openTerm(PROLOG_COMMAND_NAME);
+		pto.printAtom(path);
+		pto.closeTerm();
+	}
+
+	@Override
+	public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
+		// There are no output variables.
+	}
+
+}
