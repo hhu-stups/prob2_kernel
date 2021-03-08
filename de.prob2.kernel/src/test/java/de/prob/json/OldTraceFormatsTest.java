c367c481c953a74d82f2d34c0f58ede84c609d07
dgelessus <dgelessus@users.noreply.github.com>
2021-03-08 20:33:23 +0100

    Add tests for loading old ProB 2 UI trace files

---
+++ de.prob2.kernel/src/test/java/de/prob/json/OldTraceFormatsTest.java
@@ -0,0 +1,50 @@
+package de.prob.json;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import de.prob.check.tracereplay.json.TraceManager;
+import de.prob.check.tracereplay.json.storage.TraceJsonFile;
+import de.prob.cli.CliTestCommon;
+import de.prob.scripting.Api;
+import de.prob.scripting.ModelTranslationError;
+import de.prob.statespace.StateSpace;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public final class OldTraceFormatsTest {
+	private static StateSpace stateSpace;
+	private static TraceManager traceManager;
+	
+	@BeforeAll
+	public static void beforeAll() throws IOException, ModelTranslationError {
+		stateSpace = CliTestCommon.getInjector().getInstance(Api.class).b_load(Paths.get("src", "test", "resources", "de", "prob", "testmachines", "traces", "Lift", "Lift.mch").toString());
+		traceManager = CliTestCommon.getInjector().getInstance(TraceManager.class);
+	}
+	
+	@AfterAll
+	public static void afterAll() {
+		if (stateSpace != null) {
+			stateSpace.kill();
+		}
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"v0-prob2-ui-prerelease.prob2trace",
+		"v0-prob2-ui.prob2trace",
+		"v1-prob2-ui.prob2trace",
+	})
+	public void loadOldTraceFileTest(final String traceFileName) throws IOException {
+		final Path path = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "traces", "Lift", "oldFormats", traceFileName);
+		final TraceJsonFile jsonFile = traceManager.load(path);
+		Assertions.assertNotNull(jsonFile);
+		Assertions.assertFalse(jsonFile.getTransitionList().isEmpty());
+		// TODO Check that it replays correctly
+	}
+}
