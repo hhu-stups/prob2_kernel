9c604bf379a6c8385bebadcaef7b47745b578114
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2021-04-16 16:40:53 +0200

    Add commands for VisB HTML export

---
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
@@ -0,0 +1,29 @@
+package de.prob.animator.command;
+
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.PrologTerm;
+
+public class ExportVisBForHistoryCommand extends AbstractCommand {
+
+    private static final String PROLOG_COMMAND_NAME = "prob2_export_visb_html_for_history";
+
+    private final String path;
+
+    public ExportVisBForHistoryCommand(final String path) {
+        this.path = path;
+    }
+
+    @Override
+    public void writeCommand(IPrologTermOutput pto) {
+        pto.openTerm(PROLOG_COMMAND_NAME);
+        pto.printAtom(path);
+        pto.closeTerm();
+    }
+
+    @Override
+    public void processResult(ISimplifiedROMap<String, PrologTerm> bindings) {
+        // There are no output variables.
+    }
+
+}
