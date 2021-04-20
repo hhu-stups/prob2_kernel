5935a321c7a9c7c541583e85b76130fd95988926
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2021-04-20 09:48:59 +0200

    Add stateID to ExportVisBForHistoryCommand

--- de.prob2.kernel/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
@@ -8,15 +8,19 @@ public class ExportVisBForHistoryCommand extends AbstractCommand {
 
     private static final String PROLOG_COMMAND_NAME = "prob2_export_visb_html_for_history";
 
+    private final String stateID;
+
     private final String path;
 
-    public ExportVisBForHistoryCommand(final String path) {
+    public ExportVisBForHistoryCommand(final String stateID, final String path) {
+        this.stateID = stateID;
         this.path = path;
     }
 
     @Override
     public void writeCommand(IPrologTermOutput pto) {
         pto.openTerm(PROLOG_COMMAND_NAME);
+        pto.printAtomOrNumber(stateID);
         pto.printAtom(path);
         pto.closeTerm();
     }
