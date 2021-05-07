05613d0f7b872fba5df74cdaebbb31fbf5ea02ed
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2021-05-07 15:02:07 +0200

    Pass transitionIDs to ExportVisBForHistoryCommand

--- de.prob2.kernel/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
@@ -4,23 +4,29 @@ import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.PrologTerm;
 
+import java.util.List;
+
 public class ExportVisBForHistoryCommand extends AbstractCommand {
 
     private static final String PROLOG_COMMAND_NAME = "prob2_export_visb_html_for_history";
 
-    private final String stateID;
+    private final List<String> transIDS;
 
     private final String path;
 
-    public ExportVisBForHistoryCommand(final String stateID, final String path) {
-        this.stateID = stateID;
+    public ExportVisBForHistoryCommand(final List<String> transIDS, final String path) {
+        this.transIDS = transIDS;
         this.path = path;
     }
 
     @Override
     public void writeCommand(IPrologTermOutput pto) {
         pto.openTerm(PROLOG_COMMAND_NAME);
-        pto.printAtomOrNumber(stateID);
+        pto.openList();
+        for(String transID : transIDS) {
+            pto.printAtomOrNumber(transID);
+        }
+        pto.closeList();
         pto.printAtom(path);
         pto.closeTerm();
     }
