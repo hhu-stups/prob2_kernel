d25f004bbb9b1207057de6163173476b81a5f40c
stock <sebastian.stock@uni-duesseldorf.de>
2021-06-02 12:56:10 +0200

    added test scenario

---
+++ de.prob2.kernel/src/test/java/de/prob/check/tracereplay/check/eventB/abz/SimpleEventBMachine.java
@@ -0,0 +1,80 @@
+package de.prob.check.tracereplay.check.eventB.abz;
+
+import de.prob.ProBKernelStub;
+import de.prob.check.tracereplay.check.*;
+import de.prob.check.tracereplay.check.exploration.PersistenceDelta;
+import de.prob.check.tracereplay.check.exploration.ReplayOptions;
+import de.prob.check.tracereplay.check.exploration.TraceExplorer;
+import de.prob.check.tracereplay.check.renamig.DeltaCalculationException;
+import de.prob.check.tracereplay.check.renamig.RenamingDelta;
+import de.prob.check.tracereplay.json.TraceManager;
+import de.prob.check.tracereplay.json.storage.TraceJsonFile;
+import de.prob.cli.CliTestCommon;
+import de.prob.statespace.OperationInfo;
+import de.prob.statespace.StateSpace;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+
+public class SimpleEventBMachine {
+
+	private static TraceManager traceManager;
+	private static ProBKernelStub proBKernelStub;
+
+	@BeforeAll
+	static void beforeAll() {
+		traceManager = CliTestCommon.getInjector().getInstance(TraceManager.class);
+		proBKernelStub = CliTestCommon.getInjector().getInstance(ProBKernelStub.class);
+	}
+
+	@AfterAll
+	static void afterAll() {
+		proBKernelStub.killCurrentAnimator();
+	}
+
+
+	@Test
+	public void simple_event_b_no_changes() throws IOException, DeltaCalculationException {
+
+
+		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "eventB", "PitmanController.bum");
+		StateSpace stateSpace1 = proBKernelStub.createEventB(pathStateSpace1);
+
+		TraceJsonFile jsonFile = traceManager.load(Paths.get("src", "test", "resources", "de", "prob", "testmachines", "eventB", "PitmanController.prob2trace"));
+
+
+		Map<String, OperationInfo> oldInfos = stateSpace1.getLoadedMachine().getOperations();
+		List<String> oldVars = stateSpace1.getLoadedMachine().getVariableNames();
+
+		StateSpace stateSpace2 = proBKernelStub.createEventB(pathStateSpace1);
+		Map<String, OperationInfo> newInfos = stateSpace2.getLoadedMachine().getOperations();
+		List<String> newVars = stateSpace2.getLoadedMachine().getVariableNames();
+
+
+		TraceChecker traceChecker = new TraceChecker(jsonFile.getTransitionList(),
+				oldInfos,
+				newInfos,
+				new HashSet<>(oldVars),
+				new HashSet<>(newVars),
+				pathStateSpace1.toString(),
+				CliTestCommon.getInjector(),
+				new TestUtils.StubFactoryImplementation(),
+				new ReplayOptions(),
+				new TestUtils.ProgressStubFactory());
+
+
+
+	}
+}
