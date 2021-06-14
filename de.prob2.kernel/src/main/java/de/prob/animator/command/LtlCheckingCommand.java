d7910253d6c5a5a0433742063327342ade4bf88d
dgelessus <dgelessus@users.noreply.github.com>
2021-06-14 15:53:21 +0200

    Make LTL formulas not extend AbstractEvalElement anymore
    This was a very leaky abstraction. LTL formulas are "evaluated" very
    differently than normal expressions/predicates, so they didn't work in
    many places that accept a generic IEvalElement, e. g. evaluating
    multiple formulas, subscribing, visualisation, and many other commands.
    
    In addition, "evaluating" LTL formulas didn't always check them
    completely. LtlCheckingCommand is supposed to be called in a loop until
    it doesn't return LTLNotYetFinished anymore. LTLChecker does this
    correctly, but the implementation of eval for LTL formulas only called
    the command once.

--- de.prob2.kernel/src/main/java/de/prob/animator/command/LtlCheckingCommand.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/LtlCheckingCommand.java
@@ -16,7 +16,6 @@ import de.prob.animator.domainobjects.ErrorItem;
 import de.prob.animator.domainobjects.LTL;
 import de.prob.check.CheckInterrupted;
 import de.prob.check.IModelCheckingResult;
-import de.prob.check.LTLChecker;
 import de.prob.check.LTLCounterExample;
 import de.prob.check.LTLError;
 import de.prob.check.LTLNotYetFinished;
@@ -31,7 +30,7 @@ import de.prob.prolog.term.PrologTerm;
 import de.prob.statespace.StateSpace;
 import de.prob.statespace.Transition;
 
-public final class LtlCheckingCommand extends EvaluationCommand implements
+public final class LtlCheckingCommand extends AbstractCommand implements
 		IStateSpaceModifier {
 	private static final String PROLOG_COMMAND_NAME = "prob2_do_ltl_modelcheck";
 	private static final String VARIABLE_NAME_RESULT = "R";
@@ -47,7 +46,6 @@ public final class LtlCheckingCommand extends EvaluationCommand implements
 	private final StateSpace s;
 
 	public LtlCheckingCommand(final StateSpace s, final LTL ltlFormula, final int max) {
-		super(ltlFormula, null);
 		this.s = s;
 		this.ltlFormula = ltlFormula;
 		this.max = max;
@@ -64,23 +62,19 @@ public final class LtlCheckingCommand extends EvaluationCommand implements
 		if (term.hasFunctor("ok", 0)) {
 			LTLOk res = new LTLOk(ltlFormula);
 			result = res;
-			value = res;
 		} else if (term.hasFunctor("nostart", 0)) {
 			LTLError res = new LTLError(ltlFormula,
 					"Could not find initialisation. Try to animating the model.");
 			result = res;
-			value = res;
 		} else if (term.hasFunctor("typeerror", 0)) {
 			ListPrologTerm errorTerm = (ListPrologTerm) bindings.get(VARIABLE_NAME_ERRORS);
 			LTLError res = new LTLError(ltlFormula, String.join("\n", errorTerm.stream()
 				.map(PrologTerm::atomicString)
 				.collect(Collectors.toList())));
 			result = res;
-			value = res;
 		} else if (term.hasFunctor("incomplete", 0)) {
 			LTLNotYetFinished res = new LTLNotYetFinished(ltlFormula);
 			result = res;
-			value = res;
 		} else if (term.hasFunctor("counterexample", 3)) {
 			CompoundPrologTerm cpt = BindingGenerator.getCompoundTerm(term, 3);
 			List<Transition> counterExample = BindingGenerator.getList(cpt.getArgument(1)).stream()
@@ -116,7 +110,6 @@ public final class LtlCheckingCommand extends EvaluationCommand implements
 			
 			LTLCounterExample res = new LTLCounterExample(ltlFormula, s, pathToCE, counterExample, loopEntry, pathType);
 			result = res;
-			value = res;
 		} else {
 			throw new UnknownLtlResult("Unknown result from LTL checking: " + term);
 		}
@@ -134,7 +127,7 @@ public final class LtlCheckingCommand extends EvaluationCommand implements
 	@Override
 	public void writeCommand(final IPrologTermOutput pto) {
 		pto.openTerm(PROLOG_COMMAND_NAME);
-		evalElement.printProlog(pto);
+		this.ltlFormula.printProlog(pto);
 		pto.printNumber(max);
 		pto.printVariable(VARIABLE_NAME_RESULT);
 		pto.printVariable(VARIABLE_NAME_ERRORS);
--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/LTL.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/LTL.java
@@ -2,21 +2,16 @@ package de.prob.animator.domainobjects;
 
 import de.be4.classicalb.core.parser.ClassicalBParser;
 import de.be4.ltl.core.parser.LtlParseException;
-import de.prob.animator.command.EvaluationCommand;
-import de.prob.animator.command.LtlCheckingCommand;
 import de.prob.ltl.parser.LtlParser;
-import de.prob.model.representation.FormulaUUID;
-import de.prob.model.representation.IFormulaUUID;
 import de.prob.parserbase.ProBParserBase;
 import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.PrologTerm;
-import de.prob.statespace.State;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LTL extends AbstractEvalElement {
-	private final FormulaUUID uuid = new FormulaUUID();
+public class LTL {
+	private final String code;
 	private final PrologTerm generatedTerm;
 	
 	private static final Logger logger = LoggerFactory.getLogger(LTL.class);
@@ -27,43 +22,31 @@ public class LTL extends AbstractEvalElement {
 
 	public LTL(final String code, ProBParserBase languageSpecificParser)
 			throws LtlParseException {
-		super(code, FormulaExpand.EXPAND);
+		this.code = code;
 		generatedTerm = new de.be4.ltl.core.parser.LtlParser(languageSpecificParser)
 							.generatePrologTerm(code, "root");
 	}
 	
 	public LTL(final String code, ProBParserBase languageSpecificParser, LtlParser parser) {
-		super(code, FormulaExpand.EXPAND);
+		this.code = code;
 		generatedTerm = parser.generatePrologTerm("root", languageSpecificParser);
 	}
-		
+	
+	public String getCode() {
+		return this.code;
+	}
+	
 	@Override
+	public String toString() {
+		return this.getCode();
+	}
+	
 	public void printProlog(final IPrologTermOutput pout) {
 		if(generatedTerm == null) {
 			logger.error("PrologTerm is null");
 			return;
 		}
 		pout.printTerm(generatedTerm);
-	}
-
-	@Override
-	public EvalElementType getKind() {
-		throw new UnsupportedOperationException("Should never be called on an LTL");
-	}
-
-	@Override
-	public String serialized() {
-		throw new UnsupportedOperationException("LTL formulas cannot be serialized");
-	}
-
-	@Override
-	public IFormulaUUID getFormulaId() {
-		return uuid;
-	}
-
-	@Override
-	public EvaluationCommand getCommand(final State stateid) {
-		return new LtlCheckingCommand(stateid.getStateSpace(), this, 500);
 	}
 
 	public static LTL parseEventB(String formula) throws LtlParseException {
