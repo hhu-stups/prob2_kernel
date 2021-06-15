798b8bbb9406bb56770cb77d0574c6addf4e263b
dgelessus <dgelessus@users.noreply.github.com>
2021-06-15 14:04:01 +0200

    Add comments to LTL class to explain the different parsers a bit

--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/LTL.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/LTL.java
@@ -12,10 +12,26 @@ public class LTL {
 	private final String code;
 	private final PrologTerm generatedTerm;
 	
+	/**
+	 * Parse an LTL formula using the SableCC-based LTL parser.
+	 * Predicates in the formula are parsed using the classical B parser
+	 * (with no DEFINITIONS loaded).
+	 * 
+	 * @param code the LTL formula to parse
+	 * @throws LtlParseException if parsing failed
+	 */
 	public LTL(final String code) throws LtlParseException {
 		this(code, new ClassicalBParser());
 	}
-
+	
+	/**
+	 * Parse an LTL formula using the SableCC-based LTL parser.
+	 * Predicates in the formula are parsed using the given language-specific parser.
+	 * 
+	 * @param code the LTL formula to parse
+	 * @param languageSpecificParser the language-specific parser to use for predicates in the formula
+	 * @throws LtlParseException if parsing failed
+	 */
 	public LTL(final String code, ProBParserBase languageSpecificParser)
 			throws LtlParseException {
 		this.code = code;
@@ -23,6 +39,28 @@ public class LTL {
 							.generatePrologTerm(code, "root");
 	}
 	
+	// FIXME This API is not very nice.
+	// At the moment it is only used in the ProB 2 UI.
+	// We should move some code from the UI to here
+	// so that it is possible to parse LTL formulas using the ANTLR-based parser
+	// without having to set up the entire parser by hand.
+	/**
+	 * Construct an LTL formula from an existing instance of the ANTLR-based LTL parser.
+	 * 
+	 * Note that this constructor does <i>not</i> actually parse anything!
+	 * The calling code is expected to create an {@link LtlParser},
+	 * call {@link LtlParser#parse()},
+	 * and check that parsing was successful
+	 * (using an error listener).
+	 * Only then can this constructor be called successfully.
+	 * 
+	 * @param code the source code that was passed to the parser
+	 *     (must be passed manually, because {@link LtlParser} doesn't allow retrieving the source code that it parsed)
+	 * @param languageSpecificParser the language-specific parser to use for predicates in the formula
+	 * @param parser an instance of the ANTLR-based LTL parser,
+	 *     which must have already parsed successfully
+	 * @throws ProBError if the passed parsed instance has not parsed yet or encountered errors
+	 */
 	public LTL(final String code, ProBParserBase languageSpecificParser, LtlParser parser) {
 		this.code = code;
 		generatedTerm = parser.generatePrologTerm("root", languageSpecificParser);
@@ -43,11 +81,26 @@ public class LTL {
 	public void printProlog(final IPrologTermOutput pout) {
 		pout.printTerm(generatedTerm);
 	}
-
+	
+	/**
+	 * Parse an LTL formula using the SableCC-based LTL parser.
+	 * Predicates in the formula are parsed using the Event-B parser.
+	 *
+	 * @param formula the LTL formula to parse
+	 * @throws LtlParseException if parsing failed
+	 */
 	public static LTL parseEventB(String formula) throws LtlParseException {
 		return new LTL(formula, new EventBParserBase());
 	}
-
+	
+	/**
+	 * Parse an LTL formula using the SableCC-based LTL parser.
+	 * Predicates in the formula are parsed using the classical B parser
+	 * (with no DEFINITIONS loaded).
+	 *
+	 * @param formula the LTL formula to parse
+	 * @throws LtlParseException if parsing failed
+	 */
 	public static LTL parseClassicalB(String formula) throws LtlParseException {
 		return new LTL(formula, new ClassicalBParser());
 	}
