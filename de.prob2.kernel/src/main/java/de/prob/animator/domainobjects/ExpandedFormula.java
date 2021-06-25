1285c570f8bc65f8273ecbb82ccc1921c4e44343
dgelessus <dgelessus@users.noreply.github.com>
2021-06-25 18:19:13 +0200

    Add proof info to BVisual2 ExpandedFormula

--- de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/ExpandedFormula.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/domainobjects/ExpandedFormula.java
@@ -2,11 +2,13 @@ package de.prob.animator.domainobjects;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.google.common.base.MoreObjects;
 
 import de.prob.parser.BindingGenerator;
+import de.prob.prolog.term.CompoundPrologTerm;
 import de.prob.prolog.term.PrologTerm;
 import de.prob.statespace.State;
 import de.prob.statespace.StateSpace;
@@ -30,10 +32,88 @@ public final class ExpandedFormula {
 		}
 	}
 
+	public static final class ProofInfo {
+		private final int unchangedCount;
+		private final int provenCount;
+		private final int unprovenCount;
+		
+		public ProofInfo(final int unchangedCount, final int provenCount, final int unprovenCount) {
+			this.unchangedCount = unchangedCount;
+			this.provenCount = provenCount;
+			this.unprovenCount = unprovenCount;
+		}
+		
+		public static ExpandedFormula.ProofInfo fromPrologTerm(final PrologTerm term) {
+			int unchangedCount = 0;
+			int provenCount = 0;
+			int unprovenCount = 0;
+			
+			for (final PrologTerm entryTerm : BindingGenerator.getList(term)) {
+				final CompoundPrologTerm entry = BindingGenerator.getCompoundTerm(entryTerm, "-", 2);
+				switch (PrologTerm.atomicString(entry.getArgument(1))) {
+					case "unchanged":
+						unchangedCount = BindingGenerator.getInteger(entry.getArgument(2)).getValue().intValue();
+						break;
+					
+					case "proven":
+						provenCount = BindingGenerator.getInteger(entry.getArgument(2)).getValue().intValue();
+						break;
+					
+					case "unproven":
+						unprovenCount = BindingGenerator.getInteger(entry.getArgument(2)).getValue().intValue();
+						break;
+				}
+			}
+			
+			return new ExpandedFormula.ProofInfo(unchangedCount, provenCount, unprovenCount);
+		}
+		
+		public int getUnchangedCount() {
+			return unchangedCount;
+		}
+		
+		public int getProvenCount() {
+			return provenCount;
+		}
+		
+		public int getUnprovenCount() {
+			return unprovenCount;
+		}
+		
+		@Override
+		public boolean equals(final Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null || this.getClass() != obj.getClass()) {
+				return false;
+			}
+			final ExpandedFormula.ProofInfo other = (ExpandedFormula.ProofInfo)obj;
+			return this.getUnchangedCount() == other.getUnchangedCount()
+				&& this.getProvenCount() == other.getProvenCount()
+				&& this.getUnprovenCount() == other.getUnprovenCount();
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.getUnchangedCount(), this.getProvenCount(), this.getUnprovenCount());
+		}
+		
+		@Override
+		public String toString() {
+			return MoreObjects.toStringHelper(this)
+				.add("unchangedCount", unchangedCount)
+				.add("provenCount", provenCount)
+				.add("unprovenCount", unprovenCount)
+				.toString();
+		}
+	}
+
 	public static class Builder {
 		BVisual2Formula formula;
 		String label;
 		String description;
+		ExpandedFormula.ProofInfo proofInfo;
 		String functorSymbol;
 		List<String> rodinLabels;
 		BVisual2Value value;
@@ -45,6 +125,7 @@ public final class ExpandedFormula {
 			this.formula = null;
 			this.label = null;
 			this.description = null;
+			this.proofInfo = null;
 			this.value = null;
 			this.type = null;
 			this.subformulas = null;
@@ -72,6 +153,14 @@ public final class ExpandedFormula {
 				throw new IllegalStateException("description already set");
 			}
 			this.description = description;
+			return this;
+		}
+		
+		public ExpandedFormula.Builder proofInfo(final ExpandedFormula.ProofInfo proofInfo) {
+			if (this.proofInfo != null) {
+				throw new IllegalStateException("proofInfo already set");
+			}
+			this.proofInfo = proofInfo;
 			return this;
 		}
 		
@@ -136,6 +225,7 @@ public final class ExpandedFormula {
 	private final String label;
 	private final String description;
 	private final String functorSymbol;
+	private final ExpandedFormula.ProofInfo proofInfo;
 	private final List<String> rodinLabels;
 	private final BVisual2Value value;
 	private final FormulaType type;
@@ -156,6 +246,7 @@ public final class ExpandedFormula {
 		this.label = builder.label;
 		
 		this.description = builder.description;
+		this.proofInfo = builder.proofInfo;
 		this.functorSymbol = builder.functorSymbol;
 		this.rodinLabels = builder.rodinLabels;
 		this.value = builder.value;
@@ -198,6 +289,10 @@ public final class ExpandedFormula {
 				
 				case "description":
 					builder.description(PrologTerm.atomicString(arg));
+					break;
+				
+				case "proof_info":
+					builder.proofInfo(ExpandedFormula.ProofInfo.fromPrologTerm(arg));
 					break;
 				
 				case "functor_symbol":
@@ -247,6 +342,10 @@ public final class ExpandedFormula {
 		return description;
 	}
 	
+	public ExpandedFormula.ProofInfo getProofInfo() {
+		return this.proofInfo;
+	}
+	
 	public String getFunctorSymbol() {
 		return this.functorSymbol;
 	}
@@ -287,6 +386,7 @@ public final class ExpandedFormula {
 			.add("formula", this.getFormula())
 			.add("label", this.getLabel())
 			.add("description", this.getDescription())
+			.add("proofInfo", this.getProofInfo())
 			.add("functorSymbol", this.getFunctorSymbol())
 			.add("rodinLabels", this.getRodinLabels())
 			.add("value", this.getValue())
