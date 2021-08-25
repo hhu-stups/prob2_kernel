f760ae31ee4be7cb7b051b7a1e8170f2dd68c14a
sebastian <sebastian.stock@jku.at>
2021-08-25 14:02:18 +0200

    fixed broken tests for horizontal trace refinement

--- de.prob2.kernel/src/test/java/de/prob/check/tracereplay/check/refinement/RefinementCheckerTest.java
+++ de.prob2.kernel/src/test/java/de/prob/check/tracereplay/check/refinement/RefinementCheckerTest.java
@@ -15,7 +15,6 @@ import de.be4.classicalb.core.parser.node.Start;
 import de.be4.classicalb.core.parser.util.PrettyPrinter;
 import de.prob.ProBKernelStub;
 import de.prob.check.tracereplay.PersistentTransition;
-import de.prob.check.tracereplay.check.TraceCheckerUtils;
 import de.prob.check.tracereplay.check.traceConstruction.AdvancedTraceConstructor;
 import de.prob.check.tracereplay.check.traceConstruction.TraceConstructionError;
 import de.prob.check.tracereplay.json.TraceManager;
@@ -135,14 +134,15 @@ public class RefinementCheckerTest {
 		Assertions.assertEquals(11, nameListWithoutSkip.size());
 
 	}
-/*
+
 	@Test
 	public void test_horizontal_refinement_simple_promotes() throws IOException, TraceConstructionError, BCompoundException {
-		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotes", "M2.mch");
+		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotes", "M1.mch");
+		Path pathStateSpace2 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotes", "M2.mch");
 
 		TraceJsonFile jsonFile = traceManager.load(Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b",  "promotes", "test.prob2trace"));
 
-		TraceRefiner traceRefiner = new TraceRefiner(CliTestCommon.getInjector(), jsonFile.getTransitionList(), pathStateSpace1, pathStateSpace1);
+		TraceRefiner traceRefiner = new TraceRefiner(CliTestCommon.getInjector(), jsonFile.getTransitionList(), pathStateSpace1, pathStateSpace2);
 
 		List<PersistentTransition> result = traceRefiner.horizontalRefinement();
 
@@ -151,24 +151,26 @@ public class RefinementCheckerTest {
 
 	@Test
 	public void test_horizontal_refinement_internal_function() throws IOException, TraceConstructionError, BCompoundException {
-		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "internalOperationCall", "M2.mch");
+		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "internalOperationCall", "M1.mch");
+		Path pathStateSpace2 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "internalOperationCall", "M2.mch");
 
 		TraceJsonFile jsonFile = traceManager.load(Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b",  "internalOperationCall", "test.prob2trace"));
 
-		TraceRefiner traceRefiner = new TraceRefiner(CliTestCommon.getInjector(), jsonFile.getTransitionList(), pathStateSpace1, pathStateSpace1);
+		TraceRefiner traceRefiner = new TraceRefiner(CliTestCommon.getInjector(), jsonFile.getTransitionList(), pathStateSpace1, pathStateSpace2);
 
 		List<PersistentTransition> result = traceRefiner.horizontalRefinement();
 
 		Assertions.assertEquals(jsonFile.getTransitionList().size(), result.size());
 	}
-/*
+
 	@Test
 	public void test_horizontal_refinement_promotes_and_internal_function() throws IOException, TraceConstructionError, BCompoundException {
-		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotedAndInternal", "M2.mch");
+		Path pathStateSpace1 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotedAndInternal", "M1.mch");
+		Path pathStateSpace2 = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotedAndInternal", "M2.mch");
 
 		TraceJsonFile jsonFile = traceManager.load(Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b",  "promotedAndInternal", "test.prob2trace"));
 
-		TraceRefiner traceRefiner = new TraceRefiner(CliTestCommon.getInjector(), jsonFile.getTransitionList(), pathStateSpace1, pathStateSpace1);
+		TraceRefiner traceRefiner = new TraceRefiner(CliTestCommon.getInjector(), jsonFile.getTransitionList(), pathStateSpace1, pathStateSpace2);
 
 		List<PersistentTransition> result = traceRefiner.horizontalRefinement();
 
@@ -176,7 +178,7 @@ public class RefinementCheckerTest {
 	}
 
 	@Test
-	public void handlePromotedOperations_test_extends() throws IOException, BCompoundException {
+	public void handlePromotedOperations_test() throws IOException, BCompoundException {
 
 		Path alpha = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotes", "M3.mch");
 		Path beta = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "promotes", "M4.mch");
@@ -196,8 +198,80 @@ public class RefinementCheckerTest {
 				TraceRefiner.handlePromotedOperations(operationsFinder.getPromoted(), alphaName, new ArrayList<>(stateSpace2.getLoadedMachine().getOperations().keySet()), operationsFinder.getExtendedMachines(), operationsFinder.getIncludedImportedMachines());
 
 
-//		System.out.println("bla");
+		OperationsFinder.RenamingContainer expectedEntry = new OperationsFinder.RenamingContainer("", "Pow");
+		Map<String, OperationsFinder.RenamingContainer> expected = new HashMap<>();
+
+		expected.put("Pow", expectedEntry);
+
+		Assertions.assertEquals(expected, promotedOperations);
+
 	}
- */
+
+	@Test
+	public void handleExtendedOperations_test() throws IOException, BCompoundException {
+
+		Path alpha = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "extends", "M1.mch");
+		Path beta = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "extends", "M2.mch");
+		String alphaName = "M1";
+
+		BParser parser = new BParser(beta.toString());
+		Start betaFile = parser.parseFile(beta.toFile(), false);
+
+
+
+		OperationsFinder operationsFinder = new OperationsFinder(alphaName, betaFile);
+		operationsFinder.explore();
+
+
+		StateSpace stateSpace2 = proBKernelStub.createStateSpace(alpha);
+		Map<String, OperationsFinder.RenamingContainer> promotedOperations =
+				TraceRefiner.handlePromotedOperations(operationsFinder.getPromoted(), alphaName, new ArrayList<>(stateSpace2.getLoadedMachine().getOperations().keySet()), operationsFinder.getExtendedMachines(), operationsFinder.getIncludedImportedMachines());
+
+
+		OperationsFinder.RenamingContainer expectedEntry = new OperationsFinder.RenamingContainer("", "Inc");
+		OperationsFinder.RenamingContainer expectedEntry2 = new OperationsFinder.RenamingContainer("", "Dec");
+
+		Map<String, OperationsFinder.RenamingContainer> expected = new HashMap<>();
+
+		expected.put("Inc", expectedEntry);
+		expected.put("Dec", expectedEntry2);
+
+		Assertions.assertEquals(expected, promotedOperations);
+
+	}
+
+	@Test
+	public void handleExtendsAndRenames_test() throws IOException, BCompoundException {
+
+		Path alpha = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "extends", "M1.mch");
+		Path beta = Paths.get("src", "test", "resources", "de", "prob", "testmachines", "b", "extends", "M3.mch");
+		String alphaName = "M1";
+
+		BParser parser = new BParser(beta.toString());
+		Start betaFile = parser.parseFile(beta.toFile(), false);
+
+
+
+		OperationsFinder operationsFinder = new OperationsFinder(alphaName, betaFile);
+		operationsFinder.explore();
+
+
+		StateSpace stateSpace2 = proBKernelStub.createStateSpace(alpha);
+		Map<String, OperationsFinder.RenamingContainer> promotedOperations =
+				TraceRefiner.handlePromotedOperations(operationsFinder.getPromoted(), alphaName, new ArrayList<>(stateSpace2.getLoadedMachine().getOperations().keySet()), operationsFinder.getExtendedMachines(), operationsFinder.getIncludedImportedMachines());
+
+
+		OperationsFinder.RenamingContainer expectedEntry = new OperationsFinder.RenamingContainer("r1", "Inc");
+		OperationsFinder.RenamingContainer expectedEntry2 = new OperationsFinder.RenamingContainer("r1", "Dec");
+
+		Map<String, OperationsFinder.RenamingContainer> expected = new HashMap<>();
+
+		expected.put("Inc", expectedEntry);
+		expected.put("Dec", expectedEntry2);
+
+		Assertions.assertEquals(expected, promotedOperations);
+
+	}
+
 }
 
