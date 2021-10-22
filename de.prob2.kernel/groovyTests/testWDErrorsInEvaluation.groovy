ae188d21b1534be5b92a79c28cd080ac8ed83d35
dgelessus <dgelessus@users.noreply.github.com>
2021-10-22 18:58:01 +0200

    Fix accidental use of private field in testWDErrorsInEvaluation.groovy
    list.size on an ArrayList doesn't call the public size() method - it
    instead accesses the private field size. This "worked" on previous Java
    versions, but breaks on Java 17 with its tighter restrictions on
    reflective access to private members.

--- de.prob2.kernel/groovyTests/testWDErrorsInEvaluation.groovy
+++ de.prob2.kernel/groovyTests/testWDErrorsInEvaluation.groovy
@@ -10,13 +10,13 @@ t = t.$initialise_machine()
 // expression
 final wderror1 = t.evalCurrent("2 / 0")
 assert wderror1 instanceof WDError
-assert wderror1.errors.size == 1
+assert wderror1.errors.size() == 1
 assert wderror1.errors[0].contains("division by zero")
 
 // predicate
 final wderror2 = t.evalCurrent("3 / 0 = 1")
 assert wderror2 instanceof WDError
-assert wderror2.errors.size == 1
+assert wderror2.errors.size() == 1
 assert wderror2.errors[0].contains("division by zero")
 
 "Evaluation errors can be caught and handled simply"
