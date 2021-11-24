8dff3c6d61a507f2fa972e2c8418386cb1cad4c4
dgelessus <dgelessus@users.noreply.github.com>
2021-11-24 15:59:10 +0100

    Add ReplayTrace for holding results of trace replay command

--- de.prob2.kernel/src/main/java/de/prob/animator/command/ReplayTraceFileCommand.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/ReplayTraceFileCommand.java
@@ -1,22 +1,11 @@
 package de.prob.animator.command;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import de.prob.check.tracereplay.TraceReplayStatus;
-import de.prob.check.tracereplay.TransitionReplayPrecision;
-import de.prob.parser.BindingGenerator;
+import de.prob.check.tracereplay.ReplayedTrace;
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.PrologTerm;
-import de.prob.statespace.ITraceDescription;
-import de.prob.statespace.StateSpace;
-import de.prob.statespace.Trace;
-import de.prob.statespace.Transition;
 
-public final class ReplayTraceFileCommand extends AbstractCommand implements ITraceDescription {
+public final class ReplayTraceFileCommand extends AbstractCommand {
 	private static final String PROLOG_COMMAND_NAME = "prob2_replay_json_trace_file";
 	
 	private static final String REPLAY_STATUS_VAR = "ReplayStatus";
@@ -25,10 +14,7 @@ public final class ReplayTraceFileCommand extends AbstractCommand implements ITr
 	
 	private final String path;
 	
-	private TraceReplayStatus replayStatus;
-	private List<PrologTerm> transitionTerms;
-	private List<TransitionReplayPrecision> transitionReplayPrecisions;
-	private List<List<String>> transitionErrorMessages;
+	private ReplayedTrace trace;
 	
 	public ReplayTraceFileCommand(final String path) {
 		this.path = path;
@@ -46,44 +32,10 @@ public final class ReplayTraceFileCommand extends AbstractCommand implements ITr
 	
 	@Override
 	public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
-		this.replayStatus = TraceReplayStatus.fromPrologTerm(bindings.get(REPLAY_STATUS_VAR));
-		this.transitionTerms = BindingGenerator.getList(bindings, TRANSITIONS_VAR);
-		this.transitionReplayPrecisions = new ArrayList<>();
-		this.transitionErrorMessages = new ArrayList<>();
-		for (final PrologTerm term : BindingGenerator.getList(bindings, MATCH_INFO_LIST_VAR)) {
-			BindingGenerator.getCompoundTerm(term, "replay_step", 2);
-			this.transitionReplayPrecisions.add(TransitionReplayPrecision.fromPrologTerm(term.getArgument(1)));
-			
-			final List<String> errorMessages = new ArrayList<>();
-			for (final PrologTerm errorTerm : BindingGenerator.getList(term.getArgument(2))) {
-				if ("rerror".equals(errorTerm.getFunctor()) && errorTerm.getArity() == 1) {
-					errorMessages.add(PrologTerm.atomicString(errorTerm.getArgument(1)));
-				} else {
-					errorMessages.add(errorTerm.toString());
-				}
-			}
-			this.transitionErrorMessages.add(Collections.unmodifiableList(errorMessages));
-		}
+		this.trace = ReplayedTrace.fromProlog(bindings.get(REPLAY_STATUS_VAR), bindings.get(TRANSITIONS_VAR), bindings.get(MATCH_INFO_LIST_VAR));
 	}
 	
-	public TraceReplayStatus getReplayStatus() {
-		return this.replayStatus;
-	}
-	
-	@Override
-	public Trace getTrace(final StateSpace s) {
-		final List<Transition> transitions = this.transitionTerms.stream()
-			.map(t -> BindingGenerator.getCompoundTerm(t, "op", 4))
-			.map(t -> Transition.createTransitionFromCompoundPrologTerm(s, t))
-			.collect(Collectors.toList());
-		return new Trace(s).addTransitions(transitions);
-	}
-	
-	public List<TransitionReplayPrecision> getTransitionReplayPrecisions() {
-		return Collections.unmodifiableList(this.transitionReplayPrecisions);
-	}
-	
-	public List<List<String>> getTransitionErrorMessages() {
-		return Collections.unmodifiableList(this.transitionErrorMessages);
+	public ReplayedTrace getTrace() {
+		return trace;
 	}
 }
---
+++ de.prob2.kernel/src/main/java/de/prob/check/tracereplay/ReplayedTrace.java
@@ -0,0 +1,69 @@
+package de.prob.check.tracereplay;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.prob.parser.BindingGenerator;
+import de.prob.prolog.term.PrologTerm;
+import de.prob.statespace.ITraceDescription;
+import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
+import de.prob.statespace.Transition;
+
+public final class ReplayedTrace implements ITraceDescription {
+	private TraceReplayStatus replayStatus;
+	private List<PrologTerm> transitionTerms;
+	private List<TransitionReplayPrecision> transitionReplayPrecisions;
+	private List<List<String>> transitionErrorMessages;
+	
+	public ReplayedTrace(final TraceReplayStatus replayStatus, final List<PrologTerm> transitionTerms, final List<TransitionReplayPrecision> transitionReplayPrecisions, final List<List<String>> transitionErrorMessages) {
+		this.replayStatus = replayStatus;
+		this.transitionTerms = transitionTerms;
+		this.transitionReplayPrecisions = transitionReplayPrecisions;
+		this.transitionErrorMessages = transitionErrorMessages;
+	}
+	
+	public static ReplayedTrace fromProlog(final PrologTerm replayStatusTerm, final PrologTerm transitionTerms, final PrologTerm matchInfoTerms) {
+		final TraceReplayStatus replayStatus = TraceReplayStatus.fromPrologTerm(replayStatusTerm);
+		final List<TransitionReplayPrecision> transitionReplayPrecisions = new ArrayList<>();
+		final List<List<String>> transitionErrorMessages = new ArrayList<>();
+		for (final PrologTerm term : BindingGenerator.getList(matchInfoTerms)) {
+			BindingGenerator.getCompoundTerm(term, "replay_step", 2);
+			transitionReplayPrecisions.add(TransitionReplayPrecision.fromPrologTerm(term.getArgument(1)));
+			
+			final List<String> errorMessages = new ArrayList<>();
+			for (final PrologTerm errorTerm : BindingGenerator.getList(term.getArgument(2))) {
+				if ("rerror".equals(errorTerm.getFunctor()) && errorTerm.getArity() == 1) {
+					errorMessages.add(PrologTerm.atomicString(errorTerm.getArgument(1)));
+				} else {
+					errorMessages.add(errorTerm.toString());
+				}
+			}
+			transitionErrorMessages.add(Collections.unmodifiableList(errorMessages));
+		}
+		return new ReplayedTrace(replayStatus, BindingGenerator.getList(transitionTerms), transitionReplayPrecisions, transitionErrorMessages);
+	}
+	
+	public TraceReplayStatus getReplayStatus() {
+		return this.replayStatus;
+	}
+	
+	@Override
+	public Trace getTrace(final StateSpace s) {
+		final List<Transition> transitions = this.transitionTerms.stream()
+			.map(t -> BindingGenerator.getCompoundTerm(t, "op", 4))
+			.map(t -> Transition.createTransitionFromCompoundPrologTerm(s, t))
+			.collect(Collectors.toList());
+		return new Trace(s).addTransitions(transitions);
+	}
+	
+	public List<TransitionReplayPrecision> getTransitionReplayPrecisions() {
+		return Collections.unmodifiableList(this.transitionReplayPrecisions);
+	}
+	
+	public List<List<String>> getTransitionErrorMessages() {
+		return Collections.unmodifiableList(this.transitionErrorMessages);
+	}
+}
