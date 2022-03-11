4ab9553e03f148c44018fbd572bdc5c95045a8d0
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2022-03-11 15:26:14 +0100

    Allow to specify operation type for BeforeAfterPredicateCommand

--- de.prob2.kernel/src/main/java/de/prob/animator/command/BeforeAfterPredicateCommand.java
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/BeforeAfterPredicateCommand.java
@@ -1,5 +1,6 @@
 package de.prob.animator.command;
 
+import de.prob.animator.domainobjects.ClassicalB;
 import de.prob.animator.domainobjects.EventB;
 import de.prob.animator.domainobjects.FormulaExpand;
 import de.prob.animator.domainobjects.IEvalElement;
@@ -8,6 +9,7 @@ import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.CompoundPrologTerm;
 import de.prob.prolog.term.PrologTerm;
+import de.prob.statespace.OperationInfo;
 
 public class BeforeAfterPredicateCommand extends AbstractCommand {
 
@@ -15,10 +17,21 @@ public class BeforeAfterPredicateCommand extends AbstractCommand {
 	private static final String BA_PRED_VARIABLE = "BAPredicate";
 
 	private final String operationName;
+	private final OperationInfo.Type operationType;
 	private IEvalElement result = null;
 
 	public BeforeAfterPredicateCommand(final String operationName) {
+		// Default operation type is EventB due to historical reasons.
+		// Initially, only EventB constructs where returned. By using EventB as default,
+		// we do not break any code dependent on this command.
+		this(operationName, OperationInfo.Type.EVENTB);
+	}
+
+	public BeforeAfterPredicateCommand(
+			final String operationName,
+			final OperationInfo.Type operationType) {
 		this.operationName = operationName;
+		this.operationType = operationType;
 	}
 
 	public IEvalElement getBeforeAfterPredicate() {
@@ -29,8 +42,12 @@ public class BeforeAfterPredicateCommand extends AbstractCommand {
 	public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
 		CompoundPrologTerm compoundTerm = BindingGenerator.getCompoundTerm(bindings.get(BA_PRED_VARIABLE), 0);
 		String code = compoundTerm.getFunctor();
-		// probcli returns Unicode primes (Classical B syntax), which the Rodin parser doesn't support
-		result = new EventB(code.replace('′', '\''), FormulaExpand.EXPAND);
+		if (operationType.equals(OperationInfo.Type.EVENTB)) {
+			// probcli returns Unicode primes (Classical B syntax), which the Rodin parser doesn't support
+			result = new EventB(code.replace('′', '\''), FormulaExpand.EXPAND);
+		} else {
+			result = new ClassicalB(code, FormulaExpand.EXPAND);
+		}
 	}
 
 	@Override
