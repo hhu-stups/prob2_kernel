89a2242fa76acd3ce88d2553b3d6a0dfac2e15bc
Fabian Vu <Fabian.Vu@hhu.de>
2022-04-14 09:53:38 +0200

    Implement GetVisBSVGObjectsCommand

---
+++ de.prob2.kernel/src/main/java/de/prob/animator/command/GetVisBSVGObjectsCommand.java
@@ -0,0 +1,59 @@
+package de.prob.animator.command;
+
+
+import de.prob.animator.domainobjects.VisBSVGObject;
+import de.prob.parser.BindingGenerator;
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.ListPrologTerm;
+import de.prob.prolog.term.PrologTerm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class GetVisBSVGObjectsCommand extends AbstractCommand {
+
+	private static final String PROLOG_COMMAND_NAME = "prob2_visb_svg_objects";
+	private static final String LIST = "List";
+	private List<VisBSVGObject> svgObjects;
+
+	public GetVisBSVGObjectsCommand() {
+	}
+
+	@Override
+	public void writeCommand(final IPrologTermOutput pto) {
+		pto.openTerm(PROLOG_COMMAND_NAME);
+		pto.printVariable(LIST);
+		pto.closeTerm();
+	}
+
+	@Override
+	public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
+		final PrologTerm resultTerm = bindings.get(LIST);
+		ListPrologTerm list = (ListPrologTerm) resultTerm;
+		List<VisBSVGObject> svgObjects = new ArrayList<>();
+		for(PrologTerm svgObject : list) {
+			BindingGenerator.getCompoundTerm(svgObject, "visb_svg_object", 3);
+			String id = PrologTerm.atomicString(svgObject.getArgument(1));
+			String object = svgObject.getArgument(2).toString();
+			ListPrologTerm attributes = (ListPrologTerm) svgObject.getArgument(3);
+			Map<String, String> attributesMap = new HashMap<>();
+			for(PrologTerm attribute : attributes) {
+				BindingGenerator.getCompoundTerm(attribute, "svg_attribute", 2);
+				String key = PrologTerm.atomicString(attribute.getArgument(1));
+				String value = PrologTerm.atomicString(attribute.getArgument(2));
+				attributesMap.put(key, value);
+			}
+			svgObjects.add(new VisBSVGObject(id, object, attributesMap));
+		}
+		this.svgObjects = svgObjects;
+	}
+
+	public List<VisBSVGObject> getSvgObjects() {
+		return svgObjects;
+	}
+
+}
