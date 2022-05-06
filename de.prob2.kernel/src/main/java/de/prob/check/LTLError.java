3c2af97c3dd5f0fc3e24c9ec96b99710c1d39485
dgelessus <dgelessus@users.noreply.github.com>
2022-05-06 11:32:49 +0200

    Support errors with position info in LTL checking

--- de.prob2.kernel/src/main/java/de/prob/check/LTLError.java
+++ de.prob2.kernel/src/main/java/de/prob/check/LTLError.java
@@ -1,21 +1,38 @@
 package de.prob.check;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.prob.animator.domainobjects.ErrorItem;
 import de.prob.animator.domainobjects.LTL;
 
 public class LTLError implements IModelCheckingResult {
 
 	private final LTL formula;
-	private final String reason;
+	private final List<ErrorItem> errors;
 
-	public LTLError(final LTL formula, final String reason) {
+	public LTLError(final LTL formula, final List<ErrorItem> errors) {
 		super();
 		this.formula = formula;
-		this.reason = reason;
+		this.errors = errors;
+	}
+
+	public LTLError(final LTL formula, final String reason) {
+		this(formula, Collections.singletonList(
+			new ErrorItem(reason, ErrorItem.Type.ERROR, Collections.emptyList())
+		));
+	}
+
+	public List<ErrorItem> getErrors() {
+		return Collections.unmodifiableList(this.errors);
 	}
 
 	@Override
 	public String getMessage() {
-		return reason;
+		return this.getErrors().stream()
+			.map(ErrorItem::toString)
+			.collect(Collectors.joining("\n"));
 	}
 
 	public String getCode() {
