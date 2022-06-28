d3f26ecc0a48c3866d68b81f1ec4062c4352027c
dgelessus <dgelessus@users.noreply.github.com>
2022-06-28 13:19:32 +0200

    Add dedicated search strategy enum and field to ModelCheckingOptions
    In preparation for supporting more search strategies than the existing
    three.

--- de.prob2.kernel/src/main/java/de/prob/check/ModelCheckingOptions.java
+++ de.prob2.kernel/src/main/java/de/prob/check/ModelCheckingOptions.java
@@ -5,9 +5,21 @@ import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.google.common.base.MoreObjects;
+
 public class ModelCheckingOptions {
 	public enum Options {
+		/**
+		 * @deprecated This option has unexpected behavior in corner cases.
+		 *     Use {@link #searchStrategy(ModelCheckingSearchStrategy)} and {@link ModelCheckingSearchStrategy#BREADTH_FIRST} instead.
+		 */
+		@Deprecated
 		BREADTH_FIRST_SEARCH("breadth_first_search", "breadth first"),
+		/**
+		 * @deprecated This option has unexpected behavior in corner cases.
+		 *     Use {@link #searchStrategy(ModelCheckingSearchStrategy)} and {@link ModelCheckingSearchStrategy#DEPTH_FIRST} instead.
+		 */
+		@Deprecated
 		DEPTH_FIRST_SEARCH("depth_first_search", "depth first"),
 		FIND_DEADLOCKS("find_deadlocks", "deadlock check"),
 		FIND_INVARIANT_VIOLATIONS("find_invariant_violations", "invariant check"),
@@ -47,13 +59,16 @@ public class ModelCheckingOptions {
 	public static final ModelCheckingOptions DEFAULT = new ModelCheckingOptions()
 		.checkDeadlocks(true).checkInvariantViolations(true);
 
+	private final ModelCheckingSearchStrategy searchStrategy;
 	private final EnumSet<Options> options;
 
 	public ModelCheckingOptions() {
+		this.searchStrategy = ModelCheckingSearchStrategy.MIXED_BF_DF;
 		options = EnumSet.noneOf(Options.class);
 	}
 
-	private ModelCheckingOptions(final EnumSet<Options> options) {
+	private ModelCheckingOptions(final ModelCheckingSearchStrategy searchStrategy, final EnumSet<Options> options) {
+		this.searchStrategy = searchStrategy;
 		this.options = options;
 	}
 
@@ -63,12 +78,53 @@ public class ModelCheckingOptions {
 		if (this.options.remove(Options.FIND_OTHER_ERRORS)) {
 			this.options.add(Options.IGNORE_OTHER_ERRORS);
 		}
+		this.searchStrategy = searchStrategyFromOptions(this.options);
+	}
+	
+	private static ModelCheckingSearchStrategy searchStrategyFromOptions(final Set<Options> options) {
+		if (options.contains(Options.BREADTH_FIRST_SEARCH)) {
+			return ModelCheckingSearchStrategy.BREADTH_FIRST;
+		} else if (options.contains(Options.DEPTH_FIRST_SEARCH)) {
+			return ModelCheckingSearchStrategy.DEPTH_FIRST;
+		} else {
+			return ModelCheckingSearchStrategy.MIXED_BF_DF;
+		}
+	}
+	
+	public ModelCheckingSearchStrategy getSearchStrategy() {
+		return this.searchStrategy;
 	}
 
+	public ModelCheckingOptions searchStrategy(final ModelCheckingSearchStrategy searchStrategy) {
+		Objects.requireNonNull(searchStrategy, "searchStrategy");
+		if (this.searchStrategy == searchStrategy) {
+			return this;
+		}
+		final EnumSet<Options> newOptions = EnumSet.copyOf(this.options);
+		newOptions.remove(Options.BREADTH_FIRST_SEARCH);
+		newOptions.remove(Options.DEPTH_FIRST_SEARCH);
+		if (searchStrategy == ModelCheckingSearchStrategy.BREADTH_FIRST) {
+			newOptions.add(Options.BREADTH_FIRST_SEARCH);
+		} else if (searchStrategy == ModelCheckingSearchStrategy.DEPTH_FIRST) {
+			newOptions.add(Options.DEPTH_FIRST_SEARCH);
+		}
+		return new ModelCheckingOptions(searchStrategy, newOptions);
+	}
+
+	/**
+	 * @deprecated This method has unexpected behavior in corner cases.
+	 *     Use {@link #searchStrategy(ModelCheckingSearchStrategy)} instead.
+	 */
+	@Deprecated
 	public ModelCheckingOptions breadthFirst(final boolean value) {
 		return changeOption(value, Options.BREADTH_FIRST_SEARCH);
 	}
 	
+	/**
+	 * @deprecated This method has unexpected behavior in corner cases.
+	 *     Use {@link #searchStrategy(ModelCheckingSearchStrategy)} instead.
+	 */
+	@Deprecated
 	public ModelCheckingOptions depthFirst(final boolean value) {
 		return changeOption(value, Options.DEPTH_FIRST_SEARCH);
 	}
@@ -122,7 +178,13 @@ public class ModelCheckingOptions {
 		} else {
 			copyOf.remove(o);
 		}
-		return new ModelCheckingOptions(copyOf);
+		final ModelCheckingSearchStrategy newSearchStrategy;
+		if (o == Options.BREADTH_FIRST_SEARCH || o == Options.DEPTH_FIRST_SEARCH) {
+			newSearchStrategy = searchStrategyFromOptions(copyOf);
+		} else {
+			newSearchStrategy = this.getSearchStrategy();
+		}
+		return new ModelCheckingOptions(newSearchStrategy, copyOf);
 	}
 
 	public Set<Options> getPrologOptions() {
@@ -138,17 +200,21 @@ public class ModelCheckingOptions {
 			return false;
 		}
 		ModelCheckingOptions other = (ModelCheckingOptions) obj;
-		return other.options.equals(this.options);
+		return this.getSearchStrategy() == other.getSearchStrategy()
+			&& other.options.equals(this.options);
 	}
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(options);
+		return Objects.hash(this.getSearchStrategy(), options);
 	}
 
 	@Override
 	public String toString() {
-		return options.toString();
+		return MoreObjects.toStringHelper(this)
+			.add("searchStrategy", this.getSearchStrategy())
+			.add("options", this.options)
+			.toString();
 	}
 
 }
