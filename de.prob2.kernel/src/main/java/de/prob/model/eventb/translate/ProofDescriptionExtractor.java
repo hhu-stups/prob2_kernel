482444e277f379e2f8dcd03d4a918c35a5955048
dgelessus <dgelessus@users.noreply.github.com>
2023-01-19 18:40:16 +0100

    Preserve order of POs loaded from Rodin projects

--- de.prob2.kernel/src/main/java/de/prob/model/eventb/translate/ProofDescriptionExtractor.java
+++ de.prob2.kernel/src/main/java/de/prob/model/eventb/translate/ProofDescriptionExtractor.java
@@ -1,14 +1,14 @@
 package de.prob.model.eventb.translate;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class ProofDescriptionExtractor extends DefaultHandler {
-
-	private final Map<String, String> proofDescriptions = new HashMap<>();
+	// Use LinkedHashMap to preserve the order of proof descriptors from the Rodin project.
+	private final Map<String, String> proofDescriptions = new LinkedHashMap<>();
 
 	@Override
 	public void startElement(final String uri, final String localName,
