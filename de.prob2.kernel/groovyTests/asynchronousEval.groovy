054d95e9d0d3dc6d9085a3dcee7ec61bcf286903
dgelessus <dgelessus@users.noreply.github.com>
2023-02-22 17:44:16 +0100

    Deprecate single-formula RegisterFormulaCommand

--- de.prob2.kernel/groovyTests/asynchronousEval.groovy
+++ de.prob2.kernel/groovyTests/asynchronousEval.groovy
@@ -1,10 +1,10 @@
 import java.nio.file.Paths
 
 import de.prob.animator.command.EvaluateRegisteredFormulasCommand
-import de.prob.animator.command.RegisterFormulaCommand
+import de.prob.animator.command.RegisterFormulasCommand
 import de.prob.animator.domainobjects.ClassicalB
 import de.prob.animator.domainobjects.EvaluationErrorResult
-import de.prob.statespace.Trace;
+import de.prob.statespace.Trace
 
 final s = api.b_load(Paths.get(dir, "machines", "scheduler.mch").toString())
 
@@ -13,18 +13,16 @@ t = t.$initialise_machine()
 t = t.new("pp=PID1")
 final f = "1+1" as ClassicalB
 final f2 = "active" as ClassicalB
-final cmd1 = new RegisterFormulaCommand(f)
+final cmd1 = new RegisterFormulasCommand([f, f2])
 s.execute(cmd1)
-final cmd2 = new RegisterFormulaCommand(f2)
+final cmd2 = new EvaluateRegisteredFormulasCommand("3", [f, f2])
 s.execute(cmd2)
-final cmd3 = new EvaluateRegisteredFormulasCommand("3", [f, f2])
+assert cmd2.results[f].value == "2"
+res = cmd2.results[f2].value
+assert res == "{}" || res == "\u2205"
+final cmd3 = new EvaluateRegisteredFormulasCommand("root", [f, f2])
 s.execute(cmd3)
 assert cmd3.results[f].value == "2"
-res = cmd3.results[f2].value
-assert res == "{}" || res == "\u2205"
-final cmd4 = new EvaluateRegisteredFormulasCommand("root", [f, f2])
-s.execute(cmd4)
-assert cmd4.results[f].value == "2"
-assert cmd4.results[f2] instanceof EvaluationErrorResult
+assert cmd3.results[f2] instanceof EvaluationErrorResult
 
 "It is possible to register formulas and asynchronously evaluate them later"
