bf2e19012a17eb45c9e56bf5ef11e784fe5d0609
dgelessus <dgelessus@users.noreply.github.com>
2024-02-20 12:27:42 +0100

    Simplify Injector and model management in TraceConnectorTest

--- de.prob2.kernel/src/test/java/de/prob/check/tracereplay/check/refinement/TraceConnectorTest.java
+++ de.prob2.kernel/src/test/java/de/prob/check/tracereplay/check/refinement/TraceConnectorTest.java
@@ -1,35 +1,34 @@
 package de.prob.check.tracereplay.check.refinement;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import de.prob.MainModule;
-import de.prob.animator.ReusableAnimator;
-import de.prob.check.tracereplay.PersistentTransition;
-import de.prob.check.tracereplay.json.TraceManager;
-import de.prob.model.eventb.EventBModel;
-import de.prob.scripting.EventBFactory;
-import de.prob.statespace.StateSpace;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+import com.google.inject.Injector;
+
+import de.prob.check.tracereplay.PersistentTransition;
+import de.prob.check.tracereplay.json.TraceManager;
+import de.prob.cli.CliTestCommon;
+import de.prob.model.eventb.EventBModel;
+import de.prob.scripting.EventBFactory;
+import de.prob.scripting.ExtractedModel;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class TraceConnectorTest {
 
 	@Test
 	public void traceConnectorTest1() throws IOException {
-		Injector injector = Guice.createInjector(new MainModule());
+		Injector injector = CliTestCommon.getInjector();
 		TraceManager traceManager = injector.getInstance(TraceManager.class);
 		List<PersistentTransition> traceOld = traceManager.load(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/trace1.prob2trace").toPath()).getTransitionList();
 		List<PersistentTransition> traceNew = traceManager.load(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/trace2.prob2trace").toPath()).getTransitionList();
 
-		ReusableAnimator animator = injector.getInstance(ReusableAnimator.class);
-		StateSpace stateSpace = animator.createStateSpace();
 		EventBFactory eventBFactory = injector.getInstance(EventBFactory.class);
-		eventBFactory.extract(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/mac1.bum").toPath().toString()).loadIntoStateSpace(stateSpace);
-		EventBModel eventBModel = (EventBModel) stateSpace.getModel();
+		ExtractedModel<EventBModel> extractedModel = eventBFactory.extract(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/mac1.bum").toPath().toString());
+		extractedModel.load();
+		EventBModel eventBModel = extractedModel.getModel();
 
 		TraceConnector traceConnector = new TraceConnector(traceOld, traceNew, eventBModel.introducedBySkip());
 
@@ -41,16 +40,15 @@ public class TraceConnectorTest {
 
 	@Test
 	public void traceConnectorTest2() throws IOException {
-		Injector injector = Guice.createInjector(new MainModule());
+		Injector injector = CliTestCommon.getInjector();
 		TraceManager traceManager = injector.getInstance(TraceManager.class);
 		List<PersistentTransition> traceOld = traceManager.load(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/trace3.prob2trace").toPath()).getTransitionList();
 		List<PersistentTransition> traceNew = traceManager.load(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/trace4.prob2trace").toPath()).getTransitionList();
 
-		ReusableAnimator animator = injector.getInstance(ReusableAnimator.class);
-		StateSpace stateSpace = animator.createStateSpace();
 		EventBFactory eventBFactory = injector.getInstance(EventBFactory.class);
-		eventBFactory.extract(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/mac1.bum").toPath().toString()).loadIntoStateSpace(stateSpace);
-		EventBModel eventBModel = (EventBModel) stateSpace.getModel();
+		ExtractedModel<EventBModel> extractedModel = eventBFactory.extract(new File("src/test/resources/de/prob/testmachines/eventB/trafficLight/mac1.bum").toPath().toString());
+		extractedModel.load();
+		EventBModel eventBModel = extractedModel.getModel();
 
 		TraceConnector traceConnector = new TraceConnector(traceOld, traceNew, eventBModel.introducedBySkip());
 
