07cc12796d180a15fa3ab28d768a8d0d2b386e8d
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2024-05-14 14:27:59 +0200

    Remove buffering indirection of StructuredPrologOutput in load project commands

--- prob-java/src/main/java/de/prob/animator/command/LoadRulesProjectCommand.java
+++ prob-java/src/main/java/de/prob/animator/command/LoadRulesProjectCommand.java
@@ -3,10 +3,8 @@ package de.prob.animator.command;
 import java.io.File;
 
 import de.be4.classicalb.core.parser.rules.RulesProject;
-
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
-import de.prob.prolog.output.StructuredPrologOutput;
 import de.prob.prolog.term.PrologTerm;
 
 public class LoadRulesProjectCommand extends AbstractCommand {
@@ -23,9 +21,9 @@ public class LoadRulesProjectCommand extends AbstractCommand {
 	@Override
 	public void writeCommand(final IPrologTermOutput pto) {
 		pto.openTerm(PROLOG_COMMAND_NAME);
-		pto.printAtom(mainFile.getAbsolutePath());
+		pto.printAtom(this.mainFile.getAbsolutePath());
 		pto.openList();
-		printLoadTerm(pto);
+		this.project.printProjectAsPrologTermDirect(pto);
 		pto.closeList();
 		pto.closeTerm();
 	}
@@ -33,13 +31,5 @@ public class LoadRulesProjectCommand extends AbstractCommand {
 	@Override
 	public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
 		// There are no output variables.
-	}
-
-	private void printLoadTerm(IPrologTermOutput pto) {
-		StructuredPrologOutput parserOutput = new StructuredPrologOutput();
-		this.project.printProjectAsPrologTerm(parserOutput);
-		for (PrologTerm term : parserOutput.getSentences()) {
-			pto.printTerm(term);
-		}
 	}
 }
