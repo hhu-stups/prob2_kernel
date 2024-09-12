efefb829cbc05e26a45c6e595b3fbd61c39949ee
Jan Gruteser <jan.gruteser@hhu.de>
2024-09-12 14:18:05 +0200

    add VisBExportOptions to ExportVisBForHistoryCommand

--- prob-java/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
+++ prob-java/src/main/java/de/prob/animator/command/ExportVisBForHistoryCommand.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import de.prob.animator.domainobjects.VisBExportOptions;
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.term.PrologTerm;
@@ -14,22 +15,32 @@ public class ExportVisBForHistoryCommand extends AbstractCommand {
 	private static final String PROLOG_COMMAND_NAME = "prob2_export_visb_html_for_history";
 
 	private final List<String> transIDS;
-
+	private final VisBExportOptions options;
 	private final String path;
 
-	public ExportVisBForHistoryCommand(final List<String> transIDS, final String path) {
+	public ExportVisBForHistoryCommand(final List<String> transIDS, final VisBExportOptions options, final String path) {
 		this.transIDS = transIDS;
+		this.options = options;
 		this.path = path;
 	}
 
-	public ExportVisBForHistoryCommand(Trace trace, Path path) {
+	public ExportVisBForHistoryCommand(final Trace trace, final VisBExportOptions options, final Path path) {
 		this(
 				trace.getCurrentElements().stream()
 						.filter(e -> e.getTransition() != null)
 						.map(e -> e.getTransition().getId())
 						.collect(Collectors.toList()),
+				options,
 				path.toString()
 		);
+	}
+
+	public ExportVisBForHistoryCommand(final List<String> transIDS, final String path) {
+		this(transIDS, VisBExportOptions.DEFAULT_HISTORY, path);
+	}
+
+	public ExportVisBForHistoryCommand(final Trace trace, final Path path) {
+		this(trace, VisBExportOptions.DEFAULT_HISTORY, path);
 	}
 
 	@Override
@@ -40,6 +51,11 @@ public class ExportVisBForHistoryCommand extends AbstractCommand {
 			pto.printAtomOrNumber(transID);
 		}
 		pto.closeList();
+
+		pto.openList();
+		this.options.printProlog(pto);
+		pto.closeList();
+
 		pto.printAtom(path);
 		pto.closeTerm();
 	}
