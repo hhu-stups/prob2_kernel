4638206e0bca34317029436b64ab786d8677190c
Jan Gruteser <jan.gruteser@hhu.de>
2024-09-20 15:51:30 +0200

    add TLAModel

--- prob-java/src/main/java/de/prob/scripting/TLAFactory.java
+++ prob-java/src/main/java/de/prob/scripting/TLAFactory.java
@@ -14,7 +14,7 @@ import de.be4.classicalb.core.parser.exceptions.BCompoundException;
 import de.be4.classicalb.core.parser.exceptions.PreParseException;
 import de.be4.classicalb.core.parser.node.Start;
 import de.prob.exception.ProBError;
-import de.prob.model.classicalb.ClassicalBModel;
+import de.prob.model.representation.TLAModel;
 import de.prob.statespace.StateSpace;
 import de.tla2b.exceptions.TLA2BException;
 import de.tla2bAst.Translator;
@@ -22,22 +22,22 @@ import de.tla2bAst.Translator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TLAFactory implements ModelFactory<ClassicalBModel> {
+public class TLAFactory implements ModelFactory<TLAModel> {
 
 	private static final Logger logger = LoggerFactory.getLogger(TLAFactory.class);
 
 	private final Provider<StateSpace> stateSpaceProvider;
-	private final Provider<ClassicalBModel> modelCreator;
+	private final Provider<TLAModel> modelCreator;
 
 	@Inject
-	TLAFactory(Provider<StateSpace> stateSpaceProvider, Provider<ClassicalBModel> modelCreator) {
+	TLAFactory(Provider<StateSpace> stateSpaceProvider, Provider<TLAModel> modelCreator) {
 		this.stateSpaceProvider = stateSpaceProvider;
 		this.modelCreator = modelCreator;
 	}
 
 	@Override
-	public ExtractedModel<ClassicalBModel> extract(final String fileName) throws IOException {
-		ClassicalBModel classicalBModel = modelCreator.get();
+	public ExtractedModel<TLAModel> extract(final String fileName) throws IOException {
+		TLAModel tlaModel = modelCreator.get();
 		File f = new File(fileName);
 		if (!f.exists()) {
 			throw new FileNotFoundException("The TLA Model" + fileName + " was not found.");
@@ -65,7 +65,7 @@ public class TLAFactory implements ModelFactory<ClassicalBModel> {
 			throw new ProBError(e);
 		}
 		logger.trace("Done parsing '{}'", f.getAbsolutePath());
-		classicalBModel = classicalBModel.create(ast, rml, f, bparser);
-		return new ExtractedModel<>(stateSpaceProvider, classicalBModel);
+		tlaModel = tlaModel.create(ast, rml, f, bparser);
+		return new ExtractedModel<>(stateSpaceProvider, tlaModel);
 	}
 }
