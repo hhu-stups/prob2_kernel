7dce2ea880fa40373a440ab0ef80fbf0983d2989
dgelessus <dgelessus@users.noreply.github.com>
2025-03-13 15:22:42 +0100

    Provide Home values using ProBInstanceProvider.getProBDirectory
    This has the side effect that injecting the Home path now triggers the
    installer (if it hasn't run already) - but that's probably actually a
    good thing, because the Home path is not very useful if ProB isn't
    installed there yet.

--- prob-java/src/main/java/de/prob/cli/ModuleCli.java
+++ prob-java/src/main/java/de/prob/cli/ModuleCli.java
@@ -1,8 +1,13 @@
 package de.prob.cli;
 
+import java.io.File;
+import java.nio.file.Path;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+
+import de.prob.annotations.Home;
 
 public final class ModuleCli extends AbstractModule {
 
@@ -21,5 +26,19 @@ public final class ModuleCli extends AbstractModule {
 	@Singleton
 	private static ProBInstanceProvider getProBInstanceProvider(OsSpecificInfo osInfo) {
 		return ProBInstanceProvider.defaultProvider(osInfo);
+	}
+
+	@Provides
+	@Singleton
+	@Home
+	private static Path getProBHomePath(ProBInstanceProvider proBInstanceProvider) {
+		return proBInstanceProvider.getProBDirectory();
+	}
+
+	@Provides
+	@Singleton
+	@Home
+	private static String getProBHomeString(ProBInstanceProvider proBInstanceProvider) {
+		return proBInstanceProvider.getProBDirectory() + File.separator;
 	}
 }
