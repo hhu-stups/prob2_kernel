3c50a2ee7aaee4a040fcc2ea11a9826042e7d602
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2025-04-14 13:52:57 +0200

    Update value translator

--- de.prob2.commandline/groovyTests/valueTranslator.groovy
+++ de.prob2.commandline/groovyTests/valueTranslator.groovy
@@ -13,17 +13,17 @@ assert translate("TRUE").booleanValue() == true
 
 assert translate("FALSE").booleanValue() == false
 
-final expected1 = new BSet([new BNumber(1), new BNumber(2), new BNumber(3)].toSet())
+final expected1 = new BSet([BNumber.of(1), BNumber.of(2), BNumber.of(3)].toSet())
 assert translate("{1,2,3}") == expected1
 
-final expected2 = new BTuple(new BNumber(1), new BNumber(2))
+final expected2 = new BTuple(BNumber.of(1), BNumber.of(2))
 assert translate("1 |-> 2") == expected2
 
 assert translate("PID1") == new BAtom("PID1")
 
 assert translate('"BLAH"') == new BString("BLAH")
 
-final expected3 = new BRecord(["PID1": new BNumber(1), "PID2": new BNumber(2), "PID3": new BNumber(3)])
+final expected3 = new BRecord(["PID1": BNumber.of(1), "PID2": BNumber.of(2), "PID3": BNumber.of(3)])
 assert translate("rec(PID1:1,PID2:2,PID3:3)") == expected3
 
 "values are translated correctly"
