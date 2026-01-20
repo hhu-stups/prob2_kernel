fb7675e5c6e0a052416aff70fc8297aade3d56dd
Jan Gruteser <jan.gruteser@hhu.de>
2026-01-20 13:00:38 +0100

    refactor TransitionReplayPrecision
    so that it can also return the corresponding Prolog term

--- prob-java/src/main/java/de/prob/check/tracereplay/TransitionReplayPrecision.java
+++ prob-java/src/main/java/de/prob/check/tracereplay/TransitionReplayPrecision.java
@@ -2,33 +2,35 @@ package de.prob.check.tracereplay;
 
 import de.prob.prolog.term.PrologTerm;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum TransitionReplayPrecision {
-	FAILED,
-	KEEP_NAME,
-	PARAMETERS_ONLY,
-	PARAMETERS_AND_RESULTS,
-	PRECISE,
-	;
-	
+	FAILED("failed"),
+	KEEP_NAME("keep_name"),
+	PARAMETERS_ONLY("parameters_only"),
+	PARAMETERS_AND_RESULTS("params_and_results"),
+	PRECISE("precise");
+
+	private static final Map<String, TransitionReplayPrecision> BY_FUNCTOR = Arrays.stream(values())
+			.collect(Collectors.toMap(TransitionReplayPrecision::getPrologTerm,p -> p));
+
+	private final String prologTerm;
+
+	TransitionReplayPrecision(String prologTerm) {
+		this.prologTerm = prologTerm;
+	}
+
+	public String getPrologTerm() {
+		return prologTerm;
+	}
+
 	public static TransitionReplayPrecision fromPrologTerm(final PrologTerm term) {
-		switch (term.getFunctor()) {
-			case "failed":
-				return FAILED;
-			
-			case "keep_name":
-				return KEEP_NAME;
-			
-			case "parameters_only":
-				return PARAMETERS_ONLY;
-			
-			case "params_and_results":
-				return PARAMETERS_AND_RESULTS;
-			
-			case "precise":
-				return PRECISE;
-			
-			default:
-				throw new IllegalArgumentException("Unsupported trace replay precision: " + term);
+		TransitionReplayPrecision precision = BY_FUNCTOR.get(term.getFunctor());
+		if (precision == null) {
+			throw new IllegalArgumentException("Unsupported trace replay precision: " + term);
 		}
+		return precision;
 	}
 }
